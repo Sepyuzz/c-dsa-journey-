c2c25bec6d47f1c16439ff74a3a7ad20d25e9579
Sepyuzz <piyushmac@gamil.com>
2026-09-14 16:56:03 +0530

    solved 1st question

--- 01_basics/q01_sum.c
+++ 01_basics/q01_sum.c
@@ -1,1 +1,15 @@
+//Take two integers as input and print their sum, difference, product and quotient
 
+#include <stdio.h>
+int main(){
+    int x,y;
+    printf("enter first integer = ");
+    scanf("%d" ,&x);
+    printf("enter second integer = ");
+    scanf("%d", &y);
+    printf("sum= %d\n",x+y);
+    printf("difference = %d\n",x-y);
+    printf("product = %d\n" ,x*y);
+    printf("quotient = %f\n",(float)x/y);
+    return 0;
+}
