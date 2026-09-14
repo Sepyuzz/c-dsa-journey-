7c9c384acfcd6ccd59bdc1f3594f66ef06d2e92c
Sepyuzz <piyushmac@gamil.com>
2026-09-14 21:00:22 +0530

    solved 5th question

---
+++ 01_basics/q05_avg.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+    float t,u,v;
+    printf("enter 1st no. = ");
+    scanf("%f",&t);
+    printf("enter 2nd no. = ");
+    scanf("%f",&u);
+    printf("enter 3rd no. = ");
+    scanf("%f",&v);
+    printf("so average of these no = %f",(t+u+v)/3);
+    return 0;
+}
