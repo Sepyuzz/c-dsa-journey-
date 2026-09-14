ad9b170c397188f6d0a1b78419bf68782d617bd4
Sepyuzz <piyushmac@gamil.com>
2026-09-14 20:45:24 +0530

    solved 3rd question

---
+++ 01_basics/q03_rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter length = ");
+    scanf("%d",&a);
+    printf("enter breadth = ");
+    scanf("%d",&b);
+    printf("Area of rectangle = %d\n",a*b);
+    printf("enter length = %d",2*(a+b));
+    return 0;
+     
+}
