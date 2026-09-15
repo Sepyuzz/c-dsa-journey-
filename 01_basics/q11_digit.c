037a845a469092b49a6c581706b541dc02954be3
Sepyuzz <piyushmac@gamil.com>
2026-09-15 11:24:10 +0530

    solved 11th question

---
+++ 01_basics/q11_digit.c
@@ -0,0 +1,14 @@
+//Given an integer n, determine how 
+// many digits it contains without converting it to a string.
+#include <stdio.h>
+int main(){
+    int p,t=1,i;
+    printf("enter an amountn= ");
+    scanf("%d",&p);
+    for(i=1;i*10<=p;i=i*10){
+        t++;
+
+    }
+    printf("so no. of digits = %d",t);
+    return 0;
+}
