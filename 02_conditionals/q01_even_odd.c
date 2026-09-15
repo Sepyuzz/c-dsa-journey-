8b84bf683ba5bbe2608a640164c9fe833218af71
Sepyuzz <piyushmac0917@gmail.com>
2026-09-15 13:41:28 +0530

    solved 1st question

--- 02_conditionals/q01_even_odd.c
+++ 02_conditionals/q01_even_odd.c
@@ -1,1 +1,13 @@
-
+//Check whether a number is even or odd.
+#include <stdio.h>
+int main (){
+    int p;
+    printf("enter an integer = ");
+    scanf("%d",&p);
+    if (p%2==0){
+        printf("%d is even ",p);
+    }else {
+        printf("%d is odd ",p);
+    }
+    return 0;
+}
