8548c6eb1a094dbca5424d53f665d88aed8debe8
Sepyuzz <piyushmac0917@gmail.com>
2026-09-16 17:11:43 +0530

    solved 1st question

---
+++ 03_loops/q01_numb.c
@@ -0,0 +1,11 @@
+//Print numbers from 1 to N.
+#include <stdio.h>
+int main (){
+    int n;
+    printf("Enter n = ");
+    scanf("%d",&n);
+    for (int i =0;i<=n;i++){
+        printf("%d\n",i);
+    }
+    return 0;
+}
